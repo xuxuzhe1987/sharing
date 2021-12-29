f3025e708db2fdf92176ed9cdcba9bfbd2231e45
xuzhe xu <xuxuzhe@163.com>
2021-12-29 12:23:30 +0800

    add item model and controller

---
+++ db/migrate/20211229042232_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.string :description
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
