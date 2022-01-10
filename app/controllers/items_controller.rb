59a87eb9134961075b2952bab4591f8c8a531758
xuzhe xu <xuxuzhe@163.com>
2022-01-10 13:02:58 +0800

    pundit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,20 +2,23 @@ class ItemsController < ApplicationController
     skip_before_action :authenticate_user!, only: [ :index ]
 
     def index
-        @items = Item.all
+        @items = policy_scope(Item).order(created_at: :desc)
     end
 
     def show
         @item = Item.find(params[:id])
+        authorize @item
     end
 
     def new
         @item = Item.new 
         # needed to instantiate the form_for
+        authorize @item
     end
 
     def create
         @item = Item.new(item_params)
+        authorize @item
         @item.user = current_user
         @item.save
         # no need for app/views/items/create.html.erb
@@ -24,10 +27,12 @@ class ItemsController < ApplicationController
 
     def edit
         @item = Item.find(params[:id])
+        authorize @item
     end
 
     def update
         @item = Item.find(params[:id])
+        authorize @item
         @item.update(item_params)
         # no need for app/views/items/update.html.erb
         redirect_to item_path(@item)
@@ -35,6 +40,7 @@ class ItemsController < ApplicationController
 
     def destroy
         @item = Item.find(params[:id])
+        authorize @item
         @item.destroy
         # no need for app/views/items/destroy.html.erb
         redirect_to items_path
---
+++ app/policies/item_policy.rb
@@ -0,0 +1,32 @@
+class ItemPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+end
